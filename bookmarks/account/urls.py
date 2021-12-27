be7220088fbdd9fc1c778c11bed2b3b103acb001
Antharia <artenischloria@gmail.com>
2021-12-27 09:44:45 +0100

    FEAT User profiles

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
+
+
